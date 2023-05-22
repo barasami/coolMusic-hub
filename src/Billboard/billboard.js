596ccea91ed94c419df8ffe56bc553098bb3851e
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-05-22 22:26:21 +0300

    modified nav

--- src/Billboard/billboard.js
+++ src/Billboard/billboard.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
-const Url='https://billboard2.p.rapidapi.com/billboard_global_200'
+const Url='https://billboard-api2.p.rapidapi.com/hot-100'
 
 const options = {
-    method: 'GET',
-    params: {date: '2020-09-19'},
-    headers: {
-      'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
-      'X-RapidAPI-Host': 'billboard2.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  params: {
+    date: '2019-05-11',
+    range: '1-10'
+  },
+  headers: {
+    'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
+    'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
+  }
+};
 
 export const myBillbord=async()=>{
     try {
-        const response = await axios.get(Url,options);
+        const response= await axios.get(Url,options);
         return response
     } catch (error) {
         console.error(error);
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import HomeIcon from '@mui/icons-material/Home';
+import LyricsIcon from '@mui/icons-material/Lyrics';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='myNav'>
+        <div className='items'>
+            <div>
+                <Link to='/'><HomeIcon/>Home</Link>
+            </div>
+            <div>
+                <Link to='/Bil'><AudiotrackIcon/>BillHome</Link>
+            </div>
+            <div>
+                <Link to='/Lyric'><LyricsIcon/>Lyrics</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
