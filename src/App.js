7e1374ce6de2ada64673d95feb1356842915d06f
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-05-22 21:40:24 +0300

    added nav and footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { Fragment } from 'react';
 import './App.css';
 import Homeartist from './Artists/Homeartist';
 import Billhome from './Billboard/Billhome';
 import Lyrichome from './Lyric/Lyrichome';
+import Footer from './Footer/Footer';
+import Navbar from './Navbar/Navbar';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Homeartist/>
-      <Billhome/>
-      <Lyrichome/>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Homeartist/>}/>
+          <Route path='/Bil' element={<Billhome/>}/>
+          <Route path='/Lyric' element={<Lyrichome/>}/>
+        </Routes>
+        <Footer/>
+      </Fragment>
+    </Router>
   );
 }
 
