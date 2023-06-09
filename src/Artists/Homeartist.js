528314a6fd55ba8519debc6887d9814bdfefb183
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-06-09 22:07:56 +0300

    updated page

--- src/Artists/Homeartist.js
+++ src/Artists/Homeartist.js
@@ -27,54 +27,13 @@ function Homeartist() {
 
     let artists=artist?.artists?.items
     let albumss=artist?.items?.items
-    let episodes=artist?.episodes?.items
     let playlists=artist?.playlists?.items
-    let tracks=artist?.tracks?.items
-
-    let podcast=artist?.podcasts?.items
-
-    let topFeature=artist?.topResults?.featured
-
-    //episodes
-    const Myepisodes=episodes?.map((data)=>{
-      return(
-        <div>
-          <div className='title'>Episodes</div>
-          <div className='name'>{data.name}</div>
-          <div>{data.description}</div>
-        </div>
-      )
-    })
-
-    //podcasts
-
-    const Mypodcasts=podcast?.map((data)=>{
-      return(
-        <div>
-          <div className='title'>Podcast</div>
-          <div className='name'>{data.name}</div>
-          <div>{data.mediaType}</div>
-          <div>{data.type}</div>
-        </div>
-      )
-    })
     
-    //features
-    const Myfeature=topFeature?.map((data)=>{
-      return(
-        <div>
-          <div className='title'>Feature</div>
-          <div className='name'>{data.name}</div>
-          <div>{data.description}</div>
-          
-        </div>
-      )
-    })
 
     //albums
     const Myalbums=albumss?.map((data)=>{
       return(
-        <div>
+        <div  key={data.id} className='albums'>
           <div className='title'>Albums</div>
           <div className='name'>{data.name}</div>
           <div>{data.date.year}</div>
@@ -98,23 +57,13 @@ function Homeartist() {
 
     })
 
-    //tracks
-    const Mytracks=tracks?.map((data)=>{
-      let playable=data.playability?.playable
-      return(
-        <div>
-          <div className='title'>Tracks</div>
-          <div className='name'>{data.name}</div>
-          <div>{playable}</div>
-        </div>
-      )
-    })
+    
 
      //myartists
     const Myartists=artists?.map(({data})=>{
 
       return(
-        <div>
+        <div  key={data.id}>
           <div className='title'> Artists</div>
           <div className='name'>
             {data.profile.name}
@@ -146,21 +95,10 @@ function Homeartist() {
           <div className='artist'>
             {Myartists}
           </div>
-          <div className='tracks'>
-            {Mytracks}
-          </div>
-          <div>
-            {Myfeature}
-          </div>
           <div>
             {Myalbums}
           </div>
-          <div>
-            {Myepisodes}
-          </div>
-          <div>
-            {Mypodcasts}
-          </div>
+         
       </div>
       }
       </div>
